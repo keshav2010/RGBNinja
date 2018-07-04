36bead57de69da37b8db8577b40749c6f83c934d
keshav2010 <sharmakeshav15157@gmail.com>
2018-07-04 15:37:40 +0530

    added comment for checkRoom()

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-
+    
 const gameServer = require('./gameServer.js');
 const httpServer = http.createServer(app);
 
