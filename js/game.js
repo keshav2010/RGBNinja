40324a60d06c53240299a4266930f56c62dad8ba
keshav2010 <sharmakeshav15157@gmail.com>
2018-07-22 19:27:55 +0530

    updated sliders position, minor changes

--- js/game.js
+++ js/game.js
@@ -8,87 +8,81 @@ Use : http://kvazars.com/littera/ to generate bitmap font's xml file (.fnt / xml
 const VIEW_WIDTH = 1024;
 const VIEW_HEIGHT = 768;
 var target;
-var game = new Phaser.Game(VIEW_WIDTH, VIEW_HEIGHT, Phaser.CANVAS, '',
-{
-  init: function(){
-    console.log("game>init for socket "+Client.socket.id);
-    //basic configuration
-    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.scale.maxWidth = VIEW_WIDTH;
-    this.scale.maxHeight = VIEW_HEIGHT;
-    this.scale.pageAlignHorizontally = true;    
-    this.scale.pageAlignVertically = true;
-    this.scale.updateLayout();
-    this.stage.disableVisibilityChange = true;
-  },
-  preload : function()
-  {
-    console.log('game>preload');
-    this.state.add('Game', Game);
-    this.state.add('GameOver', GameOver);
-    this.game.load.bitmapFont('loginTitle', '/assets/fonts/pixograd.png', '/assets/fonts/pixograd.fnt');
-  },    
-  create : function()
-  {
-    console.log('game>create');
-    
-    this.game.stage.backgroundColor = "rgb("+getRand(50, 150)+","+getRand(0, 60)+","+getRand(100, 200)+")";
-    this.loginText = game.add.bitmapText(this.game.world.centerX - 200, 25, 'loginTitle', 'RGB\nNinja', 72);
-    this.loginText.text = 'rgb ninja';
-    console.log("gamejs > game > create > socket is : "+Client.socket.id);
-    //error ; this is never triggered, socket.on() doesn't work
-    Client.socket.on("startGame", function(targetRGBValue){
-        target = targetRGBValue;
-        game.state.start('Game');              
-    });
-  }
+var game = new Phaser.Game(VIEW_WIDTH, VIEW_HEIGHT, Phaser.CANVAS, '', {
+    init: function () {
+        console.log("game>init for socket " + Client.socket.id);
+        //basic configuration
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.maxWidth = VIEW_WIDTH;
+        this.scale.maxHeight = VIEW_HEIGHT;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.scale.updateLayout();
+        this.stage.disableVisibilityChange = true;
+    },
+    preload: function () {
+        console.log('game>preload');
+        this.state.add('Game', Game);
+        this.state.add('GameOver', GameOver);
+        this.game.load.bitmapFont('loginTitle', '/assets/fonts/pixograd.png', '/assets/fonts/pixograd.fnt');
+    },
+    create: function () {
+        console.log('game>create');
+
+        this.game.stage.backgroundColor = "rgb(" + getRand(50, 150) + "," + getRand(0, 60) + "," + getRand(100, 200) + ")";
+        this.loginText = game.add.bitmapText(this.game.world.centerX - 200, 25, 'loginTitle', 'RGB\nNinja', 72);
+        this.loginText.text = 'rgb ninja';
+        console.log("gamejs > game > create > socket is : " + Client.socket.id);
+        //error ; this is never triggered, socket.on() doesn't work
+        Client.socket.on("startGame", function (targetRGBValue) {
+            target = targetRGBValue;
+            game.state.start('Game');
+        });
+    }
 });
 
 
 //Main Level goes here, where actual gameplay takes place
 var targetRGBDisplay;
-var btnRed,btnGreen, btnBlue;
+var btnRed, btnGreen, btnBlue;
 var redKey, blueKey, greenKey;
 var gameReference;
-class Game extends Phaser.State
-{
-    init( targetRGBValue )
-    {
+class Game extends Phaser.State {
+    init(targetRGBValue) {
         gameReference = this;
         this.gfx = this.game.add.graphics(0, 0); //responsible for rendering on each frame
-        this.roomGfx = this.game.add.graphics(0,0); //renders only once, not on each frame
+        this.roomGfx = this.game.add.graphics(0, 0); //renders only once, not on each frame
         redKey = game.input.keyboard.addKey(Phaser.Keyboard.R);
         greenKey = game.input.keyboard.addKey(Phaser.Keyboard.G);
         blueKey = game.input.keyboard.addKey(Phaser.Keyboard.B);
         this.userCircle = {
-            posx : VIEW_WIDTH/2 - VIEW_WIDTH/4,
-            posy : VIEW_HEIGHT - 200,
-            displayColor : 0x000000,
-            updateColor : function(data){
+            posx: VIEW_WIDTH / 2 - VIEW_WIDTH / 4,
+            posy: VIEW_HEIGHT - 200,
+            displayColor: 0x000000,
+            updateColor: function (data) {
                 this.displayColor = data;
             },
-            render : function(g){
-                g.beginFill( this.displayColor, 1);
+            render: function (g) {
+                g.beginFill(this.displayColor, 1);
                 g.drawCircle(this.posx, this.posy, 200);
                 g.endFill();
             }
         };
         this.opponentCircle = {
-            posx : VIEW_WIDTH/2 + VIEW_WIDTH/4,
-            posy : VIEW_HEIGHT - 200,
-            displayColor : 0x000000,
-            updateColor : function(data){
+            posx: VIEW_WIDTH / 2 + VIEW_WIDTH / 4,
+            posy: VIEW_HEIGHT - 200,
+            displayColor: 0x000000,
+            updateColor: function (data) {
                 this.displayColor = data;
             },
-            render : function(g){
-                g.beginFill( this.displayColor, 1);
+            render: function (g) {
+                g.beginFill(this.displayColor, 1);
                 g.drawCircle(this.posx, this.posy, 200);
                 g.endFill();
             }
         };
-        
-        this.SliderBar = function(x, y, fc, nfc, w)
-        {
+
+        this.SliderBar = function (x, y, fc, nfc, w) {
             this.posx = x;
             this.posy = y;
             this.fillColor = fc;
@@ -96,187 +90,176 @@ class Game extends Phaser.State
             this.MaxValue = 255; //required by getSliderValue() fxn
             this.sliderWidth = w;
             this.sliderReference = this;
-            this.knob={};
+            this.knob = {};
         };
-        this.SliderBar.knob = function(x, y){
+        this.SliderBar.knob = function (x, y) {
             this.posx = x;
             this.posy = y;
         }
-        this.SliderBar.prototype.setColor = function( _fillCol, _noFillCol){
-                this.fillColor = _fillCol;
-                this.noFillColor = _noFillCol;
+        this.SliderBar.prototype.setColor = function (_fillCol, _noFillCol) {
+            this.fillColor = _fillCol;
+            this.noFillColor = _noFillCol;
         };
-        this.SliderBar.prototype.getSliderValue = function(){
-                return this.MaxValue *(this.knob.posx-this.posx)/(this.sliderWidth);
+        this.SliderBar.prototype.getSliderValue = function () {
+            return this.MaxValue * (this.knob.posx - this.posx) / (this.sliderWidth);
         };
-        this.SliderBar.prototype.setPos = function(_x, _y){
-                this.posx = _x;
-                this.posy = _y;
-                this.knob.setPos( _x, _y);
+        this.SliderBar.prototype.setPos = function (_x, _y) {
+            this.posx = _x;
+            this.posy = _y;
+            this.knob.setPos(_x, _y);
         };
-        this.SliderBar.prototype.setSize = function( _size){
-                this.sliderWidth = _size;
+        this.SliderBar.prototype.setSize = function (_size) {
+            this.sliderWidth = _size;
         };
-        
-        this.SliderBar.knob.prototype.moveKnob = function( _delta, slider)
-        {
+
+        this.SliderBar.knob.prototype.moveKnob = function (_delta, slider) {
             //if change is negative, ignore
-            if( _delta < 0)
+            if (_delta < 0)
                 return;
             //if change exceeds out of the limit, set knob.posX to max value
-            
-            if( _delta + this.posx > slider.posx + slider.sliderWidth){
+
+            if (_delta + this.posx > slider.posx + slider.sliderWidth) {
                 this.posx = slider.posx + slider.sliderWidth;
                 return;
             }
             //else add change into knob.posX if within constraints limit
             this.posx += _delta;
         };
-        
-        this.SliderBar.knob.prototype.setPos = function( _x, _y){
-                this.posx = _x;
-                this.posy = _y;
+
+        this.SliderBar.knob.prototype.setPos = function (_x, _y) {
+            this.posx = _x;
+            this.posy = _y;
         };
-        
-        this.SliderBar.prototype.render = function(g){
-            
+
+        this.SliderBar.prototype.render = function (g) {
+
             //render base rect
             g.beginFill(this.noFillColor, 1);
-            g.drawRect(this.posx, this.posy, 
-                                this.sliderWidth, 50);
+            g.drawRect(this.posx, this.posy,
+                this.sliderWidth, 50);
             g.endFill();
-            
+
             //render fill rect
             g.beginFill(this.fillColor, 1);
-            g.drawRect(this.posx, this.posy, 
-                              this.knob.posx - this.posx, 50);
+            g.drawRect(this.posx, this.posy,
+                this.knob.posx - this.posx, 50);
             g.endFill();
-            
 
-            
             //render knob
             g.beginFill(0xFFFFFF, 1);
-            g.drawRoundedRect(this.knob.posx-5, this.knob.posy, 10, 50, 4);
+            g.drawRoundedRect(this.knob.posx - 5, this.knob.posy, 10, 50, 4);
             g.endFill();
-            
-            
         };
         //end of sliderbar
-    }//END OF INIT()
-    preload()
-    {
+    } //END OF INIT()
+    preload() {
         console.log("game>preload");
         btnRed = this.game.add.button();
         this.game.load.bitmapFont('userNameFont', '/assets/fonts/pixograd.png', '/assets/fonts/pixograd.fnt');
         this.game.load.bitmapFont('opponentNameFont', '/assets/fonts/pixograd.png', '/assets/fonts/pixograd.fnt');
         
-        this.redSlider = new this.SliderBar(VIEW_WIDTH/2 - VIEW_WIDTH/4, VIEW_HEIGHT - 600, 0x2FF2FF, 0x000000, 200);
-        this.redSlider.knob = new this.SliderBar.knob( this.redSlider.posx, this.redSlider.posy);
-    
-        this.greenSlider = new this.SliderBar(VIEW_WIDTH/2 - VIEW_WIDTH/4, VIEW_HEIGHT - 400, 0x2FFFFF, 0x000000, 200);
-        this.greenSlider.knob = new this.SliderBar.knob( this.greenSlider.posx, this.greenSlider.posy);
-        
-        this.blueSlider = new this.SliderBar(VIEW_WIDTH/2 - VIEW_WIDTH/4, VIEW_HEIGHT - 200, 0x5FFFFF, 0x000000, 200);
-        this.blueSlider.knob = new this.SliderBar.knob( this.blueSlider.posx, this.blueSlider.posy);
+        var diff =500;
+        this.redSlider = new this.SliderBar(VIEW_WIDTH / 2 - VIEW_WIDTH / 4 - 100, VIEW_HEIGHT - diff, 0xFF0000, 0x000000, 200);
+        this.redSlider.knob = new this.SliderBar.knob(this.redSlider.posx, this.redSlider.posy);
+
+        this.greenSlider = new this.SliderBar(VIEW_WIDTH / 2 - VIEW_WIDTH / 4 - 100, VIEW_HEIGHT - diff + 54, 0x008000, 0x000000, 200);
+        this.greenSlider.knob = new this.SliderBar.knob(this.greenSlider.posx, this.greenSlider.posy);
+
+        this.blueSlider = new this.SliderBar(VIEW_WIDTH / 2 - VIEW_WIDTH / 4 - 100, VIEW_HEIGHT - diff + 108, 0x0000FF, 0x000000, 200);
+        this.blueSlider.knob = new this.SliderBar.knob(this.blueSlider.posx, this.blueSlider.posy);
     }
-    create()
-    {
+    create() {
         console.log("game>create");
         this.game.stage.backgroundColor = "rgb(210,210,210)";
-        
-        console.log("color : "+ fullColorHex(target.r, target.g, target.b));
-        
+
+        console.log("color : " + fullColorHex(target.r, target.g, target.b));
+
         //draw a line for seperation
         this.roomGfx.lineStyle(20, 0x022551);
-        this.roomGfx.moveTo(VIEW_WIDTH/2,0);
-        this.roomGfx.lineTo(VIEW_WIDTH/2, VIEW_HEIGHT);
-        
+        this.roomGfx.moveTo(VIEW_WIDTH / 2, 0);
+        this.roomGfx.lineTo(VIEW_WIDTH / 2, VIEW_HEIGHT);
+
         //draw circle to display the target rgb to acheive 
         this.roomGfx.beginFill(fullColorHex(target.r, target.b, target.g), 1);
-        this.roomGfx.drawCircle(VIEW_WIDTH/2,125, 200);
+        this.roomGfx.drawCircle(VIEW_WIDTH / 2, 125, 200);
         this.roomGfx.endFill();
-        
-        console.log("game>create>socet id : "+Client.socket.id);
-        
+
+        console.log("game>create>socet id : " + Client.socket.id);
+
         this.userNameFont = this.game.add.bitmapText(this.game.world.centerX - 200, 25, 'userNameFont', 'You', 32);
         this.opponentNameFont = this.game.add.bitmapText(this.game.world.centerX + 200, 25, 'opponentNameFont', 'Opponent', 32);
-        
-        
-        Client.socket.on('opponentAction', function(data){
+
+
+        Client.socket.on('opponentAction', function (data) {
             //convert data to hexadecimal 
             var col = fullColorHex(data[0], data[1], data[2]);
             gameReference.opponentCircle.updateColor(col);
         });
-        Client.socket.on('clientAction', function(data){
+        Client.socket.on('clientAction', function (data) {
             var col = fullColorHex(data[0], data[1], data[2]);
-            gameReference.userCircle.updateColor(col); 
+            gameReference.userCircle.updateColor(col);
         });
-        
+
     }
-    update()
-    {
-        if(redKey.isDown){
+    update() {
+        if (redKey.isDown) {
             console.log('red key pressed');
             this.redSlider.knob.moveKnob(1, this.redSlider);
-            if(this.redSlider.getSliderValue() < 250)
+            if (this.redSlider.getSliderValue() < 250)
                 Client.sendUserInput('input', 'red');
         }
-        if(greenKey.isDown){
+        if (greenKey.isDown) {
             console.log('green key pressed');
             this.greenSlider.knob.moveKnob(1, this.greenSlider);
-            if(this.greenSlider.getSliderValue() < 250)
-                Client.sendUserInput('input','green');
+            if (this.greenSlider.getSliderValue() < 250)
+                Client.sendUserInput('input', 'green');
         }
-        if(blueKey.isDown){
+        if (blueKey.isDown) {
             console.log('blue key pressed');
-            
+
             this.blueSlider.knob.moveKnob(1, this.blueSlider);
-            if(this.blueSlider.getSliderValue() < 250)
-                Client.sendUserInput('input','blue');
+            if (this.blueSlider.getSliderValue() < 250)
+                Client.sendUserInput('input', 'blue');
         }
         this.gfx.clear();
         this.userCircle.render(this.gfx);
         this.opponentCircle.render(this.gfx);
-        
+
         //sliders
         this.redSlider.render(this.gfx);
         this.greenSlider.render(this.gfx);
         this.blueSlider.render(this.gfx);
     }
-    
+
 }
 
 //the final screen
-class GameOver extends Phaser.State
-{
-  preload()
-  {
-      
-  }
-  create()
-  {
+class GameOver extends Phaser.State {
+    preload() {
 
-  }
-  update()
-  {
+    }
+    create() {
 
-  }
+    }
+    update() {
+
+    }
 }
 
 //helper methods
-const getRand = function(low, high){
-  return Math.floor((Math.random() * high) + low);
+const getRand = function (low, high) {
+    return Math.floor((Math.random() * high) + low);
 }
-var rgbToHex = function (rgb) { 
-  var hex = Number(rgb).toString(16);
-  if (hex.length < 2) {
-       hex = "0" + hex;
-  }
-  return hex;
+var rgbToHex = function (rgb) {
+    var hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+        hex = "0" + hex;
+    }
+    return hex;
 }
-var fullColorHex = function(r,g,b) {   
-  var red = rgbToHex(r);
-  var green = rgbToHex(g);
-  var blue = rgbToHex(b);
-  return parseInt(red+green+blue, 16);
+var fullColorHex = function (r, g, b) {
+    var red = rgbToHex(r);
+    var green = rgbToHex(g);
+    var blue = rgbToHex(b);
+    return parseInt(red + green + blue, 16);
 }
